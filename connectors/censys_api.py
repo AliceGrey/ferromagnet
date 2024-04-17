77c61363395d3aec634522a850f3512e9d2d21ea
Allie <allieroblee@cyberresilience.com>
2024-04-17 01:18:42 +0000

    More comments

--- connectors/censys_api.py
+++ connectors/censys_api.py
@@ -2,35 +2,41 @@
 # CYBR-260-45
 # Censys api module
 
+import time
 from censys.search import CensysHosts
 from censys.common.exceptions import CensysException
 
 def search(query, CENSYS_API_ID, CENSYS_API_SECRET):
     """
     # function: search
-    # purpose: 
-    # inputs: 
-    # returns:
+    # purpose: Use the Censys API to search their database of internet scan data
+    # inputs: The search query, a censys api id, and a censys api secret
+    # returns: A dictionary of ip/port pairs found in the search results
     """
     ip_port_pairs = {}
     try:
+        all_results = []
         count = 0
+        cursor = None
         api = CensysHosts(CENSYS_API_ID, CENSYS_API_SECRET)
 
-        all_results = []
-
-        cursor = None
+        # Send API queries while there are still pages of results to get
         while True:
-            # print(f'{query} {cursor}')
             results = api.search(query, cursor=cursor)
 
+            # Append search results
             for result in results:
                 all_results.extend(result)
 
+            # Get the next result page cursor
             cursor = results.nextCursor
             if not cursor:
                 break
 
+            # Handle API rate limit
+            time.sleep(2)
+        
+        # Extract the IP/Port pairs from the search results
         for host in all_results:
             ip = host['ip']
             count += 1
@@ -42,6 +48,8 @@ def search(query, CENSYS_API_ID, CENSYS_API_SECRET):
                 ip_port_pairs[ip].append(port)
            
         print(f'{count} Results From Censys Query - {query}')
+    
     except CensysException as e:
         print(f"Error: {e}")
+
     return ip_port_pairs
--- connectors/shodan_api.py
+++ connectors/shodan_api.py
@@ -8,25 +8,27 @@ import time
 def search(query, SHODAN_API_KEY):
     """
     # function: search
-    # purpose: 
-    # inputs: 
-    # returns:
+    # purpose: Use the Shodan API to search their database of internet scan data
+    # inputs: The search query, and a shodan api key
+    # returns: A dictionary of ip/port pairs found in the search results
     """
     ip_port_pairs = {}
     try:
-
         count = 0
-        api = Shodan(SHODAN_API_KEY)
-
         total_pages = 1 # We always get at least one page
         page = 1
+        api = Shodan(SHODAN_API_KEY)
+
         while page <= total_pages:
             results = api.search(query, page=page)
+            # Calculate the total number of pages based on each page having no more than 100 results.
             total_pages = math.ceil(results['total'] / 100)
+            # Handle API rate limit
             time.sleep(1)
 
             page += 1
 
+            # Extract the IP/Port pairs from the search results
             for result in results['matches']:
                 ip = result['ip_str']
                 count += 1
@@ -36,6 +38,8 @@ def search(query, SHODAN_API_KEY):
                 ip_port_pairs[ip].append(port)
 
         print(f"{count} Results From Shodan Query - {query}")
+
     except KeyError as e:
         print('Error: {}'.format(e))
+    
     return ip_port_pairs
